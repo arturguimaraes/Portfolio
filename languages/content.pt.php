d95c53fea4f03de1a35dfdd8d384dfced922d07e
Artur Guimarães <arturguimaraes92@hotmail.com>
2017-05-05 12:22:41 -0300

    Commit 1

---
+++ languages/content.pt.php
@@ -0,0 +1,51 @@
+<?php
+	/*
+	------------------
+	Language: Português
+	------------------
+	*/
+	$lang = array();
+
+	$lang[''] = '';
+	
+	$lang['PAGE_TITLE'] = 'Artur Guimarães | Desenvolvedor Web';
+	$lang['NAME'] = 'Artur Guimarães';
+	$lang['PROFESSION'] = 'Desenvolvedor Web';
+	$lang['PORTFOLIO'] = 'Portfolio';
+	$lang['WORK'] = 'Profissional';
+	$lang['ABOUT'] = 'Sobre';
+	$lang['CONTACT'] = 'Contato';
+	/*
+	------------------
+	Home
+	------------------
+	*/
+	$lang['AD_TITLE'] = 'Precisa criar um site?';
+	$lang['AD'] = 'Faça seu negócio crescer!<br><br>Seja para um site pessoal ou de uma empresa, é imprescindível inserir-se no mundo virtual para crescer.<br><br>A personalização vai de acordo com o cliente, sendo possível a criação de um website responsivo e com diversas funcionalidades, como Design Responsivo e Versão Mobile, garantindo o desenvolvimento de um website completo.<br><br>Não deixe seu negócio ficar no passado! Construa seu site do jeito que preferir e se integre ao mundo moderno!';
+	$lang['HOMECAROUSEL'] = 'Versões mobile!';
+	/*
+	------------------
+	Work
+	------------------
+	*/
+	$lang['EDS_TITLE'] = 'Extreme Digital Solutions';
+	$lang['EDS'] = 'Atualmente, trabalho como Desenvolvedor Web na <a href="http://extremedigital.com.br/">Extreme Digital Solutions</a>.<br><br>A EDS é uma empresa prestadora de serviços de TI. Meu setor é responsável por prestar serviço de fornecimento de suporte ao cliente. Para este, utilizamos o Oracle Service Cloud (mais conhecido como RightNow). Sou responsável pela gerência de alguns ambientes do RightNow da Oracle e o desenvolvimento dos portais web dos clientes.<br><br>Para mais informações sobre a EDS, clique no link ou na imagem acima.';
+	$lang['MUNDIPAGG_TITLE'] = 'Mundipagg';
+	$lang['MUNDIPAGG'] = 'Trabalhei como estagiário por 6 meses na Mundipagg, que é uma empresa de E-commerce.<br><br>E-commerce (Electronic Commerce, ou seja, Comércio Eletrônico) é a compra ou venda de produtos e serviços, ou transferência de fundos ou dados, via uma rede virtual, que na grande maioria dos casos é a Internet. Essas transações ocorrem entre empresas e/ou clientes.<br><br>Eu trabalhei especificamente no projeto SmartWallet. O SmartWallet é uma carteira virtual que gerencia milhares de transações de empresas como Americanas.com e Submarino. Meu time era responsável pelo desenvolvimento de novas ferramentas do serviço, e manutenção das atuais, trabalhando com o Microsoft Visual Studio em C# utilizando o .NET Framework.<br><br>Para mais informações sobre a Mundipagg, clique na imagem acima.';
+	/*
+	------------------
+	About
+	------------------
+	*/
+	$lang['ABOUT_TITLE'] = 'Sobre Artur';
+	$lang['BIO'] = 'Nascido em Brasília, moro no Rio de Janeiro por quase toda minha vida. Sou graduando de Ciência da Computação pela Universidade Federal Fluminense (UFF). Além disso, estudei por um ano em Goldsmiths University of London, em Londres, Inglaterra, pelo programa Ciência Sem Fronteiras. O curso em Goldsmiths foi Ciência da Computação, focada para Business e Desenvolvimento Web.';
+	$lang['BIO_2'] = "Artur was born in Brazil, in the capital, Brasilia. He is 22 and has lived most of his life in Rio de Janeiro. When his parents got divorced and his dad went to live in Rio, Artur went with his father. That was in 2001.<br><br>Then he went to middle school, and then high school. Always addicted to computers, he never doubt his future carreer.<br><br>When Artur was in his senior year of high school, he went as an exchange student to the USA. He stayed in a small town, called Owosso, in the state of Michigan, for one year. It was one of the greatest experiences of his life.<br><br>When he came back to Brazil, he started applying to university, and since public universities in Brazil are very hard to get in, he had to study a lot fo the exams. In 2011 he entered UFF (Federal Fluminense University), and started studying Computer Science.<br><br>In his third year of college, he applied to a program called Science Without Borders. A sponsorship program from the Brazilian government, which sends students to places all over the world to study for one year. After he got aproved, he chose London as his city. Nowadays, that's where he lives, still studying computer science in Goldsmiths University of London.<br><br>If you would like to send an email to Artur, just click on his picture above the text.";
+	$lang['SKILLS_TITLE'] = 'Habilidades de Programação';
+	$lang['SKILLS'] = array('PHP','HTML','CSS','Javascript','jQuery','SQL','Java','C','C#','C++','.NET');
+	/*
+	------------------
+	About
+	------------------
+	*/
+	$lang['CONTACT_ME'] = 'Entre em Contato';	
+?>
